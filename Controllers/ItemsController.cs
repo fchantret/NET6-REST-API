0971a27582b0f3e65505c1a85f06a910a74d05b0
Florent CHANTRET <florent@chantret.com>
2022-03-17 01:33:26 +0100

    Add entity, repository, controller and GetItems route

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,26 @@
+using Catalog.Entities;
+using Catalog.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly InMemItemsRepository repository;
+
+        public ItemsController()
+        {
+            repository = new InMemItemsRepository();
+        }
+
+        // GET /items
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            var items = repository.GetItems();
+            return items;
+        }
+    }
+}
