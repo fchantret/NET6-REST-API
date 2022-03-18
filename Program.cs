d7022cfe9495fabd9c1426b21f84d3d78d67416d
Florent CHANTRET <florent@chantret.com>
2022-03-18 22:12:33 +0100

    Customize message of the health check

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using System.Net.Mime;
+using System.Text.Json;
 using Catalog.Repositories;
 using Catalog.Settings;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
@@ -52,7 +54,24 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.MapHealthChecks("/health/ready", new HealthCheckOptions{
-    Predicate = (check) => check.Tags.Contains("ready")
+    Predicate = (check) => check.Tags.Contains("ready"),
+    ResponseWriter = async(context, report) =>
+    {
+        var result = JsonSerializer.Serialize(
+           new {
+               status = report.Status.ToString(),
+               checks = report.Entries.Select(entry => new {
+                   name = entry.Key,
+                   status = entry.Value.Status.ToString(),
+                   exception = entry.Value.Exception != null ? entry.Value.Exception.Message : "none",
+                   duration = entry.Value.Duration.ToString()
+               })
+           }
+        );
+
+        context.Response.ContentType = MediaTypeNames.Application.Json;
+        await context.Response.WriteAsync(result);
+    }
 });
 
 app.MapHealthChecks("/health/live", new HealthCheckOptions{
